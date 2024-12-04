3fb4f87a04935fd06cc1fbc4bc797a5d4b51e1a3
TahaMajlesi <186780445+TahaMajlesi@users.noreply.github.com>
2024-12-03 18:43:31 -0800

    Update

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,54 +1,12 @@
 $(document).ready(function() {
-    function isInViewport(element) {
-        var elementTop = $(element).offset().top;
-        var elementBottom = elementTop + $(element).outerHeight();
-        var viewportTop = $(window).scrollTop();
-        var viewportBottom = viewportTop + $(window).height();
+    $(window).on('scroll', function() {
+        $('.translate-x-neg-full').each(function() {
+            var elementTop = $(this).offset().top;
+            var windowTop = $(window).scrollTop() + $(window).height();
 
-        return elementBottom > viewportTop && elementTop < viewportBottom;
-    }
-
-    $(window).on('scroll resize', function() {
-        if (isInViewport('#frame3div1')) {
-            $('#frame3div1').show().animate({
-                left: '0' // Move to its normal position
-            }, 1500); // Duration of the animation in milliseconds
-        }
+            if (windowTop > elementTop) {
+                $(this).removeClass('translate-x-neg-full').addClass('translate-x-0');
+            }
+        });
     });
-    $(window).on('scroll resize', function() {
-        if (isInViewport('#frame3div2')) {
-            $('#frame3div2').show().animate({
-                left: '0' // Move to its normal position
-            }, 1500); // Duration of the animation in milliseconds
-        }
-    });
-    $(window).on('scroll resize', function() {
-        if (isInViewport('#frame3div3')) {
-            $('#frame3div3').show().animate({
-                left: '0' // Move to its normal position
-            }, 1500); // Duration of the animation in milliseconds
-        }
-    });
-    $(window).on('scroll resize', function() {
-        if (isInViewport('#frame3div4')) {
-            $('#frame3div4').show().animate({
-                left: '0' // Move to its normal position
-            }, 1500); // Duration of the animation in milliseconds
-        }
-    });
-    $(window).on('scroll resize', function() {
-        if (isInViewport('#frame3div5')) {
-            $('#frame3div5').show().animate({
-                left: '0' // Move to its normal position
-            }, 1500); // Duration of the animation in milliseconds
-        }
-    });
-    $(window).on('scroll resize', function() {
-        if (isInViewport('#frame3div6')) {
-            $('#frame3div6').show().animate({
-                left: '0' // Move to its normal position
-            }, 1500); // Duration of the animation in milliseconds
-        }
-    });
-
 });
